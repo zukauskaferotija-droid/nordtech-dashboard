6b80ed4d84f45d3d36731ce29f1928ccde29048f
zukauskaferotija-droid <zukauskaferotija@gmail.com>
2026-02-22 06:19:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -105,29 +105,34 @@ with st.sidebar.expander("🔎 Filtri", expanded=True):
     selected_categories = st.multiselect(
         "Produktu kategorija",
         options=all_categories,
-        default=all_categories,
+        default=[],
         key="cat_filter"
     )
 
-    # Product Name atkarīgs no kategorijas
+    # --- Product Name atkarīgs no kategorijas ---
     if selected_categories:
-        available_products = (
-            df[df["Product_Category_clean"].isin(selected_categories)]
-            ["Product_Name_clean"]
-            .dropna()
-            .unique()
-            .tolist()
-        )
+    available_products = (
+        df[df["Product_Category_clean"].isin(selected_categories)]
+        ["Product_Name_clean"]
+        .dropna()
+        .unique()
+        .tolist()
+    )
     else:
-        available_products = df["Product_Name_clean"].dropna().unique().tolist()
+    available_products = (
+        df["Product_Name_clean"]
+        .dropna()
+        .unique()
+        .tolist()
+    )
 
     available_products = sorted(available_products)
 
-    selected_products = st.multiselect(
-        "Produkts",
-        options=available_products,
-        default=available_products,
-        key="product_filter"
+    selected_products = st.sidebar.multiselect(
+    "Produkts",
+    options=available_products,
+    default=[],
+    key="product_filter"
     )
 
     min_date = df["Date"].min()
